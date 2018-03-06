20f4dd98bd044406350bbf153aaac3fcb31542ef
husarms <husarms@gmail.com>
2018-03-06 00:48:05 -0500

    Hooking up api

--- server/server.js
+++ server/server.js
@@ -10,9 +10,10 @@ const app = express();
 // Priority serve any static files
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-// Answer API requests
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+// Answer API requests.
+app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
 });
 
 // All remaining requests return the React app, so it can handle routing.
--- src/App.js
+++ src/App.js
@@ -3,19 +3,56 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: null,
+            fetching: true
+        };
+    }
+
+    componentDidMount() {
+        fetch('/api')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`status ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(json => {
+                this.setState({
+                    message: json.message,
+                    fetching: false
+                });
+            }).catch(e => {
+            this.setState({
+                message: `API call failed: ${e}`,
+                fetching: false
+            });
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    {'This is '}
+                    <a href="https://github.com/husarms/seekjdm">
+                        {'seekjdm'}
+                    </a><br/>
+                </p>
+                <p className="App-intro">
+                    {this.state.fetching
+                        ? 'Fetching message from API'
+                        : this.state.message}
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
