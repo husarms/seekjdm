d51135d5a372242a63771fb7acdced4df13c0567
husarms <husarms@gmail.com>
2018-03-06 00:21:31 -0500

    Point to build folder in server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 //const port = process.env.PORT || 5000;
 
 //Point to our react-create-app build folder
-var buildDir = _dirname + "/build/";
+var buildDir = __dirname + "/build/";
 app.use(express.static(buildDir));
 
 app.get('/api/hello', (req, res) => {
