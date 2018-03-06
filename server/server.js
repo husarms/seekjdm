fc910e600a6ef9954c99fecacc2083b2d16146a6
husarms <husarms@gmail.com>
2018-03-06 00:41:48 -0500

    Updating server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const path = require('path');
 // const scrapeVehicles = require('./scrapers/scrape');
 // const dbHelper = require('./db/dbHelper');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
-//const port = process.env.PORT || 5000;
 
-//Point to our react-create-app build folder
-var buildDir = __dirname + "/build/";
-app.use(express.static(buildDir));
+// Priority serve any static files
+app.use(express.static(path.resolve(__dirname, '../build')));
 
+// Answer API requests
 app.get('/api/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
 });
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
+
+
 
 // app.get('/api/vehicles', (req, res) => {
 //     scrapeVehicles(function(vehicleCount){
